d9870afc8281a376fa61f2b60e342cfde31cee7d
KuzminaYuliya <kuzmina_h@mail.ru>
2021-11-14 01:05:40 +0500

    commit

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,15 +3,14 @@ import Star from "./Star";
 
 const Stars = (props) => {
   let li = [];
-  if (typeof props.count === "number" && props.count >= 0 && props.count <= 5) {
-    for (let i = 0; i < props.count; i++) {
-      li.push(<Star key={i} />);
-    }
-
-    return <ul className="ulStar">{li}</ul>;
-  } else {
+  if (!(typeof props.count === "number" && props.count >= 0 && props.count <= 5)) {
     return null;
+  }  
+    
+  for (let i = 0; i < props.count; i++) {
+    li.push(<Star key={i} />);
   }
+  return <ul className="ulStar">{li}</ul>;
 };
 
 Stars.defaultProps = {
